08d860f48ac268113a8b32db36259a7e4caa26b4
Chikombe <chikombe.ecy@gmail.com>
2024-05-11 13:59:05 +0000

    2. Best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,7 @@
--- Rank country origins of bands by number of fans
-SELECT origin, COUNT(*) AS nb_fans
+-- This SQL script selects the origin column, and the sum of fans column as nb_fans,
+-- grouped by origin and ordered by nb_fans descending from the 'metal_bands' table.
+
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
